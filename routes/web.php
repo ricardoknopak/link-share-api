1c645ebe90411304ccf50a9a1565bd1e53176b2b
Ricardo Knopak <eu@ricardok.com.br>
2022-06-09 10:09:50 -0300

    alteracoes

--- routes/web.php
+++ routes/web.php
@@ -6,7 +6,7 @@ $router->get('/', function () use ($router) {
     return $router->app->version();
 });
 
-$router->group(['prefix' => 'api', 'middleware' => 'authenticator'], function () use ($router) {
+$router->group(['prefix' => 'api'], function () use ($router) {
     $router->group(['prefix' => 'bookmarks'], function () use ($router) {
         $router->get('', 'BookmarksController@index');
         $router->get('{id:[\d]+}', 'BookmarksController@show');
@@ -14,7 +14,7 @@ $router->group(['prefix' => 'api', 'middleware' => 'authenticator'], function ()
         $router->put('{id:[\d]+}', 'BookmarksController@update');
         $router->delete('{id:[\d]+}', 'BookmarksController@destroy');
     });
-    $router->group(['prefix' => 'tags', 'middleware' => 'authenticator'], function () use ($router) {
+    $router->group(['prefix' => 'tags'], function () use ($router) {
         $router->get('', 'TagsController@index');
         $router->get('{id:[\d]+}', 'TagsController@show');
         $router->post('', 'TagsController@store');
